36201c22a6db31335ac2362c0c94b436b5a5dee6
Brandon <brandon@parksbra.com>
2025-12-02 01:03:03 -0800

    update endpoint

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -6,5 +6,5 @@ locals {
 
   velero_bucket_name = module.velero_minio_tenant.tenant_bucket_names[0]
   velero_bucket_region = module.velero_minio_tenant.tenant_bucket_regions_by_name[local.velero_bucket_name]
-  velero_bucket_endpoint = "${module.velero_minio_tenant.tenant_service_protocol}://${module.velero_minio_tenant.tenant_service_address}"
+  velero_bucket_endpoint = "${module.velero_minio_tenant.tenant_service_protocol}://${module.velero_minio_tenant.tenant_service_address}.cluster.local"
 }
