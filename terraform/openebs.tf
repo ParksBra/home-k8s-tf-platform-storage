050fe31c92db8fd9df9c911d7c47aeb6dea33793
Brandon <brandon@parksbra.com>
2025-12-01 16:25:25 -0800

    update source path

--- terraform/openebs.tf
+++ terraform/openebs.tf
@@ -1,5 +1,5 @@
 module "openebs" {
-  source = "github.com/ParksBra/home-k8s-tf-lib//modules/openebs?ref=create_platform_tf"
+  source = "github.com/ParksBra/home-k8s-tf-lib//modules/openebs_zfs?ref=create_platform_tf"
 
   namespace = data.kubernetes_namespace.openebs.metadata[0].name
   create_namespace = false
