c41c0c330abacbdcbb95a8f18b73a722a2f17df4
Brandon <brandon@parksbra.com>
2025-12-02 22:19:14 -0800

    update to test version

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,11 +2,19 @@ terraform {
   required_providers {
     helm = {
       source = "hashicorp/helm"
-      version = "~> 3"
+      version = "~> 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
     }
     jinja = {
       source = "NikolaLohinski/jinja"
-      version = "~> 2"
+      version = "~> 2.0"
     }
   }
 }
