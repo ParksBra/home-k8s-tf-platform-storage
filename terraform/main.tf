b2479a961777d597e47f25dbe1f608ff2346045d
Brandon <brandon@parksbra.com>
2025-12-03 16:42:41 -0800

    remove unused infisical provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,33 @@
-provider "infisical" {
-  host = var.infisical_url
-  auth = {
-    universal = {
-      client_id     = var.infisical_auth_client_id
-      client_secret = var.infisical_auth_client_secret
-    }
-  }
-}
+module "storage" {
+  depends_on = [
+    kubernetes_storage_class.velero,
+    kubernetes_persistent_volume.velero
+  ]
+  source = "./modules/_storage"
 
-provider "kubernetes" {
-  config_path = var.kubeconfig_path
-}
+  openebs_namespace = local.openebs_namespace
+  create_openebs_namespace = true
 
-provider "kubectl" {
-  config_path = var.kubeconfig_path
-}
+  minio_operator_namespace = local.minio_operator_namespace
+  create_minio_operator_namespace = true
 
-provider "helm" {
-  kubernetes = {
-    config_path = var.kubeconfig_path
-  }
-}
+  velero_namespace = local.velero_namespace
+  create_velero_namespace = true
 
-provider "jinja" {}
+  velero_minio_pool_storage_class_name = kubernetes_storage_class.velero.metadata[0].name
+  velero_minio_pool_size_gb = local.velero_minio_pool_size_gb
+  velero_minio_pool_server_count = local.velero_minio_pool_server_count
+  velero_minio_pool_volume_count = local.velero_minio_pool_volume_count
+  velero_minio_pool_node_selector = local.velero_minio_pool_node_selector
+  velero_minio_pool_tolerations = local.velero_minio_pool_tolerations
 
-provider "cloudflare" {
-  api_token = data.infisical_secrets.environment.secrets[local.cloudflare_provider_api_token_infisical_secret_name].value
+  velero_minio_pool_user_id = local.velero_minio_pool_user_id
+  velero_minio_pool_group_id = local.velero_minio_pool_group_id
+
+  velero_internal_kubectl_repository = local.velero_internal_kubectl_repository
+  velero_internal_kubectl_tag = local.velero_internal_kubectl_tag
+
+  velero_scheduled_backups = local.velero_scheduled_backups
+  velero_scheduled_backup_common_labels = local.velero_scheduled_backup_common_labels
+  velero_scheduled_backup_common_annotations = local.velero_scheduled_backup_common_annotations
 }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    infisical = {
-      source  = "infisical/infisical"
-      version = "~> 0.0"
-    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
