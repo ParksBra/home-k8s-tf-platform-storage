715a494e9239867e8b4daf3662bbb1454ec8338f
Brandon <brandon@parksbra.com>
2025-12-04 15:07:58 -0800

    add missing provider ref

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "~> 1.0"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
