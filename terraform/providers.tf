3ffee4eec43f30015937621e60dbf9fd062d8d71
Brandon <brandon@parksbra.com>
2025-12-16 16:15:42 -0800

    define cloudflare provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,3 +5,5 @@ provider "kubectl" {} # Configured via KUBE_CONFIG_PATH env var
 provider "helm" {} # Configured via KUBE_CONFIG_PATH env var
 
 provider "jinja" {}
+
+provider "cloudflare" {} # Configured via CLOUDFLARE_API_TOKEN env var
